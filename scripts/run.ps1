e86dd171137aca990a9d71b2f1051d53bfb68e4e
Allan <allan.mercou@icloud.com>
2018-12-07 09:59:45 +0100

    Who even have 32bit system...
    Fixes dlls and script adjustement for more verbosity.

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -8,5 +8,6 @@ else
 {
     $run = "x86"
 }
-
-javaw "-Djava.library.path=..\j3d\$run" -classpath "..\bin;..\j3d\j3dcore.jar;..\j3d\j3dutils.jar;..\j3d\vecmath.jar" game.PakMan "..\bin\levels\1.txt"
+echo $run
+java "-Djava.library.path=..\j3d\$run" -classpath "..\bin;..\j3d\j3dcore.jar;..\j3d\j3dutils.jar;..\j3d\vecmath.jar" game.PakMan "..\bin\levels\1.txt"
+pause
