77b628f7825cc5feadb74262ddf4706e5f488595
Allan <allan.mercou@icloud.com>
2018-12-11 18:27:32 +0100

    Update script, update levels

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -6,7 +6,8 @@ if ([Environment]::Is64BitOperatingSystem)
 }
 else 
 {
-    $run = "x86"
+    echo "Impossible de le lancer sur un système 32 ou 86 bits."
+    exit
 }
-echo $run
+
 javaw "-Djava.library.path=..\j3d\$run" -classpath "..\bin;..\j3d\j3dcore.jar;..\j3d\j3dutils.jar;..\j3d\vecmath.jar" game.PakMan "--from-script"
